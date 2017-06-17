4e8f7ca95f1831f89c9ee2fea5dc446359669414
Buty <buty015@gmail.com>
2017-06-17 17:57:46 +0300

    Collection View

---
+++ ismic/NewsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCollectionViewCell.swift
+//  ismic
+//
+//  Created by Muluken on 6/17/17.
+//  Copyright © 2017 GCME-EECMY. All rights reserved.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+    
+    public var indexPath:IndexPath!
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+}
